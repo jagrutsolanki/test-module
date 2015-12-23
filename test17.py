4bd7ae4f2625f335a8106fe3884fe312fa20374b
jagrut <jagrutsolanki@browseinfo.in>
2015-12-23 17:34:06 +0530

    test17.py change

--- test17.py
+++ test17.py
@@ -1,3 +1,24 @@
 def say_hi():
  print 'this is my module'
 __version__ = '0.1'
+
+ab = { 
+	'jagrut': 'jagrutsolanki1@gmail.com',
+	'hardik': 'hardikpatel1701@gmail.com',
+	'hard'  : 'hardchavda1201@gmail.com',
+	'mihir' : 'mihirmodi91@gmail.com'
+}
+
+print 'jagrut email address is:',ab['jagrut']
+
+del ab['mihir']
+
+print '\n There are {} contacts in address-book\n'.format(len(ab)) 
+
+for name,address in ab.items():
+ print 'Contact {} at {} '.format(name,address)
+
+ab['jay'] = 'jaypatel01@gmail.com'
+
+if 'jay' in ab:
+ print '\n jay address is',ab['jay']
